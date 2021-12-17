50cceef6b94affee7749a412b6a0475e0450aff7
Marcin Ostaszewski <m.ostaszewski@azagroup.eu>
2021-12-17 14:08:54 +0100

    add EventComponent

---
+++ src/events/EventComponent.tsx
@@ -0,0 +1,18 @@
+const EventComponent: React.FC = () => {
+
+    const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(event);
+    }
+    const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+        console.log(event.clientX)
+    }
+
+    return (
+        <div>
+            <input type="text" onChange={onChange} />
+            <div draggable onDragStart={onDragStart}></div>
+        </div>
+    )
+}
+
+export { EventComponent };
